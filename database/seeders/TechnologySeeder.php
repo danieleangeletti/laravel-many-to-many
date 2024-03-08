3c5bf711a2d6a3133a6dd994d6b9d7eed2e29910
danieleangeletti <angelettidaniele@gmail.com>
2024-03-08 15:47:57 +0100

    Completamento model Technology -msrR

---
+++ database/seeders/TechnologySeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+// Models
+use App\Models\Technology;
+
+// Helpers
+use Illuminate\Support\Facades\Schema;
+
+class TechnologySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Schema::disableForeignKeyConstraints();
+        Technology::truncate();
+        Schema::enableForeignKeyConstraints();
+
+        $all_technologies = [
+            'HTML',
+            'CSS',
+            'JavaScript',
+            'Vue',
+            'PHP',
+            'MySql',
+            'Laravel'
+        ];
+
+        foreach ($all_technologies as $single_technology) {
+            $single_technology = Technology::create([
+                'title' => $single_technology,
+                'slug' => Str()->slug($single_technology)
+            ]);
+        }
+    }
+}
