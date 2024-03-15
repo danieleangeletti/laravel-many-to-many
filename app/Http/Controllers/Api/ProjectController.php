5398b6b4bc660c779a81b5c94d9638a8bf772e37
danieleangeletti <angelettidaniele@gmail.com>
2024-03-15 06:52:04 +0100

    FIX api/ProjecController show

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -21,7 +21,7 @@ class ProjectController extends Controller
     }
     public function show(string $slug)
     {
-        $project = Project::with('type', 'technologies')::where('slug', $slug)->firstOrFail();
+        $project = Project::with('type', 'technologies')->where('slug', $slug)->firstOrFail();
 
         if ($project != null) {
             return response()->json([
